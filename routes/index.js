59c5691e316292913df4a6f7848bdd38474566e9
bb-worm <xofyd99@naver.com>
2020-01-10 11:21:55 +0900

    메인 이미지 보내기

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // 메인 이미지 보냄 
+  res.sendfile(path.resolve('public/images/qu.jpeg'));
 });
 
 module.exports = router;
